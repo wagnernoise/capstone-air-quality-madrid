7315e8839dc31d4c56c6bff0d9b4c1081cf26603
Wagner Rosa <wagner.deoliveira@perkinelmer.com>
2020-01-10 19:46:19 +0100

    R script and data files

---
+++ AirQualityMadrid_Capstone.r
@@ -0,0 +1,13 @@
+###########################################################################
+#                                                                         #
+#                                                                         #
+#     Capstone project - Air Quality in Madrid - From 2001 to 2018        #
+#                                                                         #
+#                                                                         #
+###########################################################################
+
+#Loading the necessary libraries
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(lubridate)) install.packages("lubridate", repos = "http://cran.us.r-project.org")
+if(!require(forecast)) install.packages("forecast", repos = "http://cran.us.r-project.org")
+
